c0b1939ad570994ae5679a8923014a80ce1b29e2
Vedang <vedangssjgod@gmail.com>
2024-07-19 21:21:22 +0530

    Added the carousel for the website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './customer/components/navigation/Navigation';
+import HomePage from './customer/components/pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <div>
-        
+        <HomePage />
       </div>
     </div>
   );
